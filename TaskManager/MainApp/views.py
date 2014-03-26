760a4f1c340544f08a060c6d38d5219c3608b176
Rafael Lott <rafael.lott2@gmail.com>
2014-03-26 21:46:00 +0200

    Creation of login page

--- TaskManager/MainApp/views.py
+++ TaskManager/MainApp/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'MainApp/home.html')
-# Create your views here.
+
+def login(request):
+    return render(request, 'MainApp/login.html')
--- TaskManager/TaskManager/urls.py
+++ TaskManager/TaskManager/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^home/', include('MainApp.urls')),
+    url(r'$', 'MainApp.views.login'),
 )
