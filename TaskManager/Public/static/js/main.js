b73072fdcbefa387681b778afea7cc387e2ef27f
iz4k <ilari.meuronen@gmail.com>
2014-03-31 14:37:43 +0300

    added active selector functionality to navbar

--- TaskManager/Public/static/js/main.js
+++ TaskManager/Public/static/js/main.js
@@ -2,6 +2,8 @@ $(function(){
 
 	$('.navbar-nav li a').click(function(){
 		$('.left-panel').load($(this).attr('href') + ' .content');
+		$('.active').removeClass('active');
+		$(this).parent('li').addClass('active');
 		return false;
 	});
 
