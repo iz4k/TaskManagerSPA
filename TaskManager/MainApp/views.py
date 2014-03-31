61852d08241ad3493a36fdb87952a7698534f907
iz4k <ilari.meuronen@gmail.com>
2014-03-31 14:29:29 +0300

    groups are listed on group page

--- TaskManager/MainApp/views.py
+++ TaskManager/MainApp/views.py
@@ -3,6 +3,8 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 
+from MainApp.models import *
+
 def home(request):
 
     if request.user.is_authenticated():
@@ -16,8 +18,9 @@ def home(request):
 def groups(request):
 
 	if request.user.is_authenticated():
-		user = request.user
-		return render_to_response('MainApp/groups.html', {'user':request.user})
+		
+		group_list = Group.objects.filter(users = request.user)
+		return render_to_response('MainApp/groups.html', {'user':request.user, 'group_list':group_list})
 	else:
 		#messages.error(request, 'User not authorized.')
 		return HttpResponseRedirect("/login")
