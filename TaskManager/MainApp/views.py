5fcb3cd84ee4ff2a768e0364d639261737dcb73c
iz4k <ilari.meuronen@gmail.com>
2014-04-01 14:51:45 +0300

    back button maybe finally yes? login_reguired to views

--- TaskManager/MainApp/views.py
+++ TaskManager/MainApp/views.py
@@ -2,64 +2,51 @@ from django.shortcuts import render
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from MainApp.models import *
 from main_forms import *
 import sys
+
+@login_required()
 def home(request):
+	user = request.user
+	return render_to_response('MainApp/home.html', {'user':request.user})
+    
 
-    if request.user.is_authenticated():
-    	print >>sys.stderr, 'TEST PRINT PLS IGNORE'
-        user = request.user
-        return render_to_response('MainApp/home.html', {'user':request.user})
-    else:
-        #messages.error(request, 'User not authorized.')
-        return HttpResponseRedirect("/login")
-
-
+@login_required()
 def groups(request):
-
-	if request.user.is_authenticated():
-		if request.is_ajax():
-			group_list = Group.objects.filter(users = request.user)
-			return render_to_response('MainApp/groups.html', {'user':request.user, 'group_list':group_list})
-		else:
-			return HttpResponseRedirect("/")
+	if request.is_ajax():
+		group_list = Group.objects.filter(users = request.user)
+		return render_to_response('MainApp/groups.html', {'user':request.user, 'group_list':group_list})
 	else:
-		#messages.error(request, 'User not authorized.')
-		return HttpResponseRedirect("/login")
+		return HttpResponseRedirect("/")
 
+@login_required()
 def groups_new(request):
-	if request.user.is_authenticated():
-		if request.method == 'POST': # If the form has been submitted...
-			form = GroupForm(request.POST) # A form bound to the POST data
-			if form.is_valid(): # All validation rules pass
-				form.save()
-				return HttpResponseRedirect("/groups") # Redirect after POST
-		else:
-			form = GroupForm() # An unbound form
-
-		#return render_to_response('MainApp/groups_new.html', context_instance=RequestContext(request, {'form': form}))
-		return render_to_response('MainApp/groups_new.html',{'user':request.user, 'form':form}, context_instance=RequestContext(request)) 
+	if request.method == 'POST': # If the form has been submitted...
+		form = GroupForm(request.POST) # A form bound to the POST data
+		if form.is_valid(): # All validation rules pass
+			form.save()
+			return HttpResponseRedirect("/groups") # Redirect after POST
 	else:
-		#messages.error(request, 'User not authorized.')
-		return HttpResponseRedirect("/login")
+		form = GroupForm() # An unbound form
 
+	#return render_to_response('MainApp/groups_new.html', context_instance=RequestContext(request, {'form': form}))
+	return render_to_response('MainApp/groups_new.html',{'user':request.user, 'form':form}, context_instance=RequestContext(request)) 
+	
+@login_required()
 def groups_view(request, group_id):
-    if request.user.is_authenticated():
-		if request.is_ajax():
-			user = request.user
-			try:
-				group = Group.objects.get(id = group_id)
-			except Group.DoesNotExist:
-				#some sort of error page here?
-				return HttpResponseRedirect("/")
-			#need to check if user is in group here
-			return render_to_response('MainApp/group.html', {'group':group})
-		else:
+	if request.is_ajax():
+		user = request.user
+		try:
+			group = Group.objects.get(id = group_id)
+		except Group.DoesNotExist:
+			#some sort of error page here?
 			return HttpResponseRedirect("/")
-    else:
-        #messages.error(request, 'User not authorized.')
-        return HttpResponseRedirect("/login")
-        # NEED TO RETURN LIST OF RELATED TASKS TOO
+		#need to check if user is in group here
+		return render_to_response('MainApp/group.html', {'group':group})
+	else:
+		return HttpResponseRedirect("/")
+    
 
--- TaskManager/Public/static/js/main.js
+++ TaskManager/Public/static/js/main.js
@@ -1,25 +1,19 @@
 $(function(){
 	window.onpopstate = function(event) {
-		$('.left-panel').load(location.pathname);
+		$('.left-panel').load(location.pathname+' .content');
 	}
 
 	$(document).on('click', '.ajax', function(){
-		$('.left-panel').load($(this).attr('href'));
-
+	
 	
 		$('.left-panel').load($(this).attr('href')+' .content');
 
 		$('.active').removeClass('active');
 		$(this).parent('li').addClass('active');
-		console.log("#");
+		console.log($(this).attr('href'));
 		history.pushState(null, null, $(this).attr('href'));
 		return false;
 	});
-
-
-	window.onpopstate = function(event) {
-		$('.left-panel').load(location.pathname);
-	}
 
 });
 
